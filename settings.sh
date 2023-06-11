c3fdc02a824048c3dd2aee5222a12ffd3917b616
Antony Messerli <antony@mes.ser.li>
2023-06-10 20:57:45 -0500

    Specify filename

--- settings.sh
+++ settings.sh
@@ -1,8 +1,8 @@
 URL="https://cdimage.debian.org/debian-cd/current-live/amd64/iso-hybrid/debian-live-REPLACE_VERSION-amd64-standard.iso"
 TYPE=file
 CONTENTS="\
-live/initrd*|initrd
-live/vmlinuz*|vmlinuz"
+live/initrd.img|initrd
+live/vmlinuz|vmlinuz"
 EXTRACT_INITRD="true"
 INITRD_NAME="initrd"
 INITRD_TYPE="gz"
