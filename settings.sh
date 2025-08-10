5c1c2d54f5a0b89dd6a14e027cff4869c59db5e7
Antony Messerli <antony@mes.ser.li>
2025-08-09 21:32:38 -0500

    Revert "Generate Debian 13 core"
    This reverts commit 5b34a6ca90a680aaeb51fb5e31672057694ae616.

--- settings.sh
+++ settings.sh
@@ -1,4 +1,4 @@
-URL="https://cdimage.debian.org/debian-cd/13.0.0-live/amd64/iso-hybrid/debian-live-REPLACE_VERSION-amd64-standard.iso"
+URL="https://cdimage.debian.org/debian-cd/current-live/amd64/iso-hybrid/debian-live-REPLACE_VERSION-amd64-standard.iso"
 TYPE=file
 CONTENTS="\
 live/initrd.img|initrd
--- version.sh
+++ version.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -e
-VERSION=$(curl -sfL https://cdimage.debian.org/debian-cd/13.0.0-live/amd64/iso-hybrid/SHA256SUMS | grep 'standard.iso' | grep -Po "(\d+\.)+\d+" | head -n1)
+VERSION=$(curl -sfL https://cdimage.debian.org/debian-cd/current-live/amd64/iso-hybrid/SHA256SUMS | grep 'standard.iso' | grep -Po "(\d+\.)+\d+" | head -n1)
 echo "${VERSION}"
